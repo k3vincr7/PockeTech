f60149ed907775b846b630c3cff11a484a09d541
k3vincr7 <k3vincr@gmail.com>
2025-09-02 06:59:29 -0500

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'pocketech-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+  // You can list icon paths here too when you add them, e.g. './icons/pwa-192.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
+  );
+});
